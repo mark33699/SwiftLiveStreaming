0e894a11af0dd7cf25d36692671d119e27d2d942
mark <mark.appledeveloper@gmail.com>
2019-05-20 21:23:12 +0800

    Initial Commit

---
+++ SwiftLiveStreaming/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftLiveStreaming
+//
+//  Created by 謝飛飛 on 2019/5/20.
+//  Copyright © 2019 謝飛飛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
